0160059bb2abb9c93c54d50745868e1a36c94b1d
Mubashir Shahzad <mubashirs1234567@gmail.com>
2026-04-05 15:57:50 +0500

    Just a commit for logging

--- Games.java
+++ Games.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 
+/**
+ * @author Mubashir Shahzad
+ * @LastModified 05 April 2026
+ */
 public class Games {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
